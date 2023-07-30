f8a3401fbbb1798d370455c3a1978566ed87d4ec
Mohamed28112003 <114874929+Mohamed28112003@users.noreply.github.com>
2023-07-30 17:16:05 +0300

    Update and rename main.py to Data Cleaning.py

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -9,14 +9,22 @@ df= pd.read_csv(r"C:\Users\mohamed22\Downloads\glassdoor_jobs.csv")
 # salary
 
 
+ # reomve -1 salaries
+df=df[df['Salary Estimate']!='-1']
 
-df=df[df['Salary Estimate']!='-1'] # reomve -1 salaries
-salary=df['Salary Estimate'].apply(lambda x:x.split('(')[0]) # reomve text from salary
-salary=salary.apply(lambda x:x.replace("$",'').replace('K','')) # replace k and $
+# reomve text from salary
+salary=df['Salary Estimate'].apply(lambda x:x.split('(')[0]) 
+
+ # replace k and $
+salary=salary.apply(lambda x:x.replace("$",'').replace('K',''))
+# per hour
 df['Salary_Per_Hour'] = salary.apply(lambda x: 1 if "per hour" in x.lower() else 0)
+
+# emp provided 
 df['Employer Provdid '] = salary.apply(lambda x: 1 if "employer provided salary" in x.lower() else 0)
 
 salary=salary.apply(lambda x:x.replace('Employer Provided Salary:','').replace('Per Hour',''))
+# min / max / avg 
 df['Min_Salary']=salary.apply(lambda x:x.split('-')[0])
 df['Max_Salary']=salary.apply(lambda x:x.split('-')[1])
 df['Avg_Salary']=(df['Max_Salary'].astype(int)+df['Min_Salary'].astype(int))/2
@@ -34,6 +42,7 @@ df['Same_Location'] = df.apply(lambda x: 1 if x.Location == x.Headquarters else 
 
 #company age
 df['Company_Age']=df['Founded'].apply(lambda x : x if x<0 else 2023-x)
+
 #jop_des
 
 #python
@@ -56,4 +65,4 @@ df['Excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() el
 df_out = df.drop(['Unnamed: 0'], axis =1)
 
 df_out.to_csv('data_cleaned.csv',index = False)
-df2= pd.read_csv('data_cleaned.csv')
+
